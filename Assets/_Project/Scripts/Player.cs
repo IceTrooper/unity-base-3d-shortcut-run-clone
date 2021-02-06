a5b4efae83b28c9508c705fba7b0a8254a189077
Ice_trooper <trooperice@gmail.com>
2021-02-06 19:35:20 +0100

    added assets; added player movement

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private bool enableMoving;
+
+    private int rotationInputDirection;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        rotationInputDirection = (int)Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if(enableMoving)
+        {
+            rb.MovePosition(rb.position + transform.forward * forwardSpeed * Time.fixedDeltaTime);
+            rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(rotationInputDirection * rotationSpeed * Time.fixedDeltaTime, Vector3.up));
+        }
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            Debug.DrawRay(transform.position, Vector3.down);
+            Debug.Log(hit.collider.tag);
+            if(hit.collider.CompareTag("Void"))
+            {
+                PlacePlank();
+            }
+        }
+    }
+
+    private void PlacePlank()
+    {
+
+    }
+}
